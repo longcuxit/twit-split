16cb027eb0e17c6279825d83a6b7a65f359e3c47
Hoàng Long <longcuxit@gmail.com>
2019-03-15 00:11:35 +0700

    Add basic split, validate message

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,18 +1,36 @@
 import React from "react";
 
+import { splitMessage } from '../utils';
+
 const Context = React.createContext({});
 
 
 export class Provider extends React.Component {
   state = { items: [], loading: true };
 
-  methods = {
-    sendMsg: (msg) => {
-      const { items } = this.state;
-      items.push(msg);
-      this.setState({ items });
+  sendMsg = (msg) => new Promise((next, error) => {
+    const { items } = this.state;
+    items.push(msg);
+    this.setState({ items });
+    next();
+  })
+
+  sendMsgs = (msgs) => new Promise((next, error) => {
+    const msg = msgs.shift();
+    if (msg) {
+      this.sendMsg(msg)
+        .then(() => this.sendMsgs(msgs))
+        .then(next)
+        .catch(error)
+    } else {
+      next();
     }
+  });
+
+  methods = {
+    sendMsg: (msg) => new Promise(next => this.sendMsgs(splitMessage(msg)).then(next))
   }
+
   get contextProps() {
     const { items } = this.state;
     return Object.assign({ items }, this.methods);
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,24 +3,33 @@ import React from 'react';
 import { connect } from './Context';
 
 class Form extends React.Component {
-  state = { msg: ''}
+  state = { msg: '', error: false }
 
   onSubmit = (e) => {
     this.props.sendMsg(this.state.msg)
+      .then(() => {
+        this.refs.input.value = '';
+      })
+      .catch(e => this.setState({ error: e.message }))
     e.preventDefault();
-    return false;
   }
 
   onTypeMsg = (e) => {
-    console.log(e.target.value)
     const { state } = this;
     state.msg = e.target.value;
+    state.error && this.setState({ error: false });
+  }
+
+  shouldComponentUpdate(props, state, a) {
+    return state.error !== this.state.error;
   }
 
   render() {
+    const { error } = this.state;
     return (
       <form onSubmit={this.onSubmit}>
-        <input onChange={this.onTypeMsg}/>
+        {error && <div>{error}</div>}
+        <input ref='input' onChange={this.onTypeMsg} multiple/>
       </form>
     )
   }

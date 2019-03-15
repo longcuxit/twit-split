1093bcbb6f1072feb5a2cf125d17043f4de12b14
Hoàng Long <longcuxit@gmail.com>
2019-03-16 03:42:12 +0700

    format list message

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import 'scroll-behaviour/polyfill';
 import { connect } from './Context';
 
 
@@ -7,15 +7,38 @@ class List extends React.Component {
   static defaultProps = {
     items: []
   }
+  cache = {
+    itemCount: 0
+  }
+  shouldComponentUpdate(props, state) {
+    const { cache } = this, { items } = props;
+    if (cache.itemCount < items.length) {
+      cache.itemCount = items.length;
+      window.requestAnimationFrame(() => {
+        const { scroller } = this.refs, endScroll = scroller.scrollHeight - scroller.offsetHeight;
+        if (endScroll > scroller.scrollTop) {
+          scroller.scrollTo({ top: endScroll, behavior: 'smooth' });
+        }
+      });
+      return true;
+    }
+  }
+
   render() {
     const { items } = this.props;
     
     return (
-      <ul className="c-list container">
-        {items.map((item, index) => {
-          return (<li key={index}>{item}</li>)
-        })}
-      </ul>
+      <section ref="scroller" className="c-list">
+        <ul className="c-list_items container">
+          {items.map((item, index) => {
+            return (
+              <li key={index} className="c-list__item">
+                <div className="msg">{item}</div>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     )
   }
 }
